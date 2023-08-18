2228592e652e90f54c2b2eee80970916d468ef5e
dammyololade <dammyololade2010@gmail.com>
2023-08-18 23:17:37 +0200

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Adedamola Adeyemo on 18.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
